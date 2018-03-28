74a970e3b2fc9f343a1df52c2f1750672a708536
jiananWang <a0918666589@gmail.com>
2018-03-28 18:57:20 +0800

    Update 2018-03-28T10:57:18.511Z

--- js/all.js
+++ js/all.js
@@ -1,2 +1,15 @@
-"use strict";var func=function(){};func();var func2=function(){};func2();
+'use strict';
+
+var func = function func() {
+    console.log('Yo, bro.');
+};
+
+func();
+'use strict';
+
+var func2 = function func2() {
+    console.log('I\'m func 2');
+};
+
+func2();
 //# sourceMappingURL=all.js.map
